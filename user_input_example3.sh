6b090df63602851d481cb6688f547432f79bf1c3
psyll0n <alex.jakimov@gmail.com>
2021-07-09 15:44:45 +0300

    Adding several scripts.

---
+++ user_input_example3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "Hello, please enter something: \c"
+read word
+echo "You entered: $word"
+echo -e "Please, enter two words separated by space"
+read word1 word2
+echo "Here are your words: \"$word1\" \"$word2\""
+echo -e "How do you feel about bash scripting?"
+read  REPLY
+echo "You said $REPLY, I'm happy to hear this!"
+echo "What are your favorite colors?"
+
+# -a reads a command in a massive
+
+read -a colours
+echo "My favorite colors are also ${colours[0]}, ${colours[1]} and ${colours[2]}:-)"
