d8b64f25e1e2d44c43c150b6b14cb6b39ce3e502
psyll0n <alex.jakimov@gmail.com>
2022-02-02 20:53:06 +0200

    Modified bash_functions2.sh

--- bash_functions2.sh
+++ bash_functions2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# bash_functions2.sh - Demonstrates the use of local and global variables in Bash functions. 
+# bash_functions2.sh - Demonstrates the use of local and global variables in Bash functions.
 
 my_func() {
     echo "I was called as : $@"
     x=2
 }
 
-### Main script starts here. 
+### Main script starts here.
 echo "Script was called with $@"
 x=1
 echo "x is $x"
